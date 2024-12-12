1018f8d4fa888e39c31669dc644129c718025180
opilane <opilane@HP-CZC2349HTZ>
2024-12-12 09:58:26 +0200

    muuda

--- Admin.Designer.cs
+++ Admin.Designer.cs
@@ -28,157 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Filmi_pealkiri_lbl = new System.Windows.Forms.Label();
-            this.Filmi_aasta_lbl = new System.Windows.Forms.Label();
-            this.filimi_nimi_txt = new System.Windows.Forms.TextBox();
-            this.filmi_aasta_txt = new System.Windows.Forms.TextBox();
-            this.lisa_poster_btn = new System.Windows.Forms.Button();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.insert_btn = new System.Windows.Forms.Button();
-            this.delete_btn = new System.Windows.Forms.Button();
-            this.update_btn = new System.Windows.Forms.Button();
-            this.saal_lbl = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // Filmi_pealkiri_lbl
-            // 
-            this.Filmi_pealkiri_lbl.AutoSize = true;
-            this.Filmi_pealkiri_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.Filmi_pealkiri_lbl.Location = new System.Drawing.Point(24, 22);
-            this.Filmi_pealkiri_lbl.Name = "Filmi_pealkiri_lbl";
-            this.Filmi_pealkiri_lbl.Size = new System.Drawing.Size(108, 20);
-            this.Filmi_pealkiri_lbl.TabIndex = 0;
-            this.Filmi_pealkiri_lbl.Text = "Filmi pealkiri";
-            // 
-            // Filmi_aasta_lbl
-            // 
-            this.Filmi_aasta_lbl.AutoSize = true;
-            this.Filmi_aasta_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.Filmi_aasta_lbl.Location = new System.Drawing.Point(24, 67);
-            this.Filmi_aasta_lbl.Name = "Filmi_aasta_lbl";
-            this.Filmi_aasta_lbl.Size = new System.Drawing.Size(96, 20);
-            this.Filmi_aasta_lbl.TabIndex = 2;
-            this.Filmi_aasta_lbl.Text = "Filmi aasta";
-            // 
-            // filimi_nimi_txt
-            // 
-            this.filimi_nimi_txt.Location = new System.Drawing.Point(164, 22);
-            this.filimi_nimi_txt.Name = "filimi_nimi_txt";
-            this.filimi_nimi_txt.Size = new System.Drawing.Size(152, 20);
-            this.filimi_nimi_txt.TabIndex = 3;
-            // 
-            // filmi_aasta_txt
-            // 
-            this.filmi_aasta_txt.Location = new System.Drawing.Point(164, 67);
-            this.filmi_aasta_txt.Name = "filmi_aasta_txt";
-            this.filmi_aasta_txt.Size = new System.Drawing.Size(152, 20);
-            this.filmi_aasta_txt.TabIndex = 4;
-            // 
-            // lisa_poster_btn
-            // 
-            this.lisa_poster_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.lisa_poster_btn.Location = new System.Drawing.Point(485, 164);
-            this.lisa_poster_btn.Name = "lisa_poster_btn";
-            this.lisa_poster_btn.Size = new System.Drawing.Size(135, 44);
-            this.lisa_poster_btn.TabIndex = 5;
-            this.lisa_poster_btn.Text = "Lisa poster";
-            this.lisa_poster_btn.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView2
-            // 
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(28, 228);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(737, 210);
-            this.dataGridView2.TabIndex = 6;
-            // 
-            // insert_btn
-            // 
-            this.insert_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.insert_btn.Location = new System.Drawing.Point(28, 164);
-            this.insert_btn.Name = "insert_btn";
-            this.insert_btn.Size = new System.Drawing.Size(136, 44);
-            this.insert_btn.TabIndex = 7;
-            this.insert_btn.Text = "Lisa andmed";
-            this.insert_btn.UseVisualStyleBackColor = true;
-            // 
-            // delete_btn
-            // 
-            this.delete_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.delete_btn.Location = new System.Drawing.Point(180, 164);
-            this.delete_btn.Name = "delete_btn";
-            this.delete_btn.Size = new System.Drawing.Size(136, 44);
-            this.delete_btn.TabIndex = 8;
-            this.delete_btn.Text = "Kustuta";
-            this.delete_btn.UseVisualStyleBackColor = true;
-            // 
-            // update_btn
-            // 
-            this.update_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.update_btn.Location = new System.Drawing.Point(333, 164);
-            this.update_btn.Name = "update_btn";
-            this.update_btn.Size = new System.Drawing.Size(135, 44);
-            this.update_btn.TabIndex = 9;
-            this.update_btn.Text = "Uuenda";
-            this.update_btn.UseVisualStyleBackColor = true;
-            // 
-            // saal_lbl
-            // 
-            this.saal_lbl.AutoSize = true;
-            this.saal_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.saal_lbl.Location = new System.Drawing.Point(24, 112);
-            this.saal_lbl.Name = "saal_lbl";
-            this.saal_lbl.Size = new System.Drawing.Size(77, 20);
-            this.saal_lbl.TabIndex = 10;
-            this.saal_lbl.Text = "Kinosaal";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(164, 111);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(152, 21);
-            this.comboBox1.TabIndex = 11;
             // 
             // Admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.saal_lbl);
-            this.Controls.Add(this.update_btn);
-            this.Controls.Add(this.delete_btn);
-            this.Controls.Add(this.insert_btn);
-            this.Controls.Add(this.dataGridView2);
-            this.Controls.Add(this.lisa_poster_btn);
-            this.Controls.Add(this.filmi_aasta_txt);
-            this.Controls.Add(this.filimi_nimi_txt);
-            this.Controls.Add(this.Filmi_aasta_lbl);
-            this.Controls.Add(this.Filmi_pealkiri_lbl);
             this.Name = "Admin";
             this.Text = "Admin";
-            this.Load += new System.EventHandler(this.Admin_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label Filmi_pealkiri_lbl;
-        private System.Windows.Forms.Label Filmi_aasta_lbl;
-        private System.Windows.Forms.TextBox filimi_nimi_txt;
-        private System.Windows.Forms.TextBox filmi_aasta_txt;
-        private System.Windows.Forms.Button lisa_poster_btn;
-        private System.Windows.Forms.DataGridView dataGridView2;
-        private System.Windows.Forms.Button insert_btn;
-        private System.Windows.Forms.Button delete_btn;
-        private System.Windows.Forms.Button update_btn;
-        private System.Windows.Forms.Label saal_lbl;
-        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- Admin.cs
+++ Admin.cs
@@ -13,10 +13,12 @@ namespace Kino_AB_V.A.TARpv23
 {
     public partial class Admin : Form
     {
-        DataGridView dataGridView1;
-        PictureBox pictureBox_admin;
-        TextBox filmiNimi_txt, filmiImg_txt, filmiAasta_txt;
-        Button kustuta_btn, lisa_btn, uuenda_btn;
+        Label Filmi_pealkiri_lbl, Filmi_aasta_lbl, saal_lbl;
+        TextBox filimi_nimi_txt, filmi_aasta_txt;
+        Button lisa_poster_btn, insert_btn, delete_btn, update_btn;
+        DataGridView dataGridView;
+        ComboBox comboBox1;
+        DataTable kinosaal_table;
         SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\opilane\source\repos\ValeriaAllikTARpv23\Kino_AB_V.A.TARpv23\Database_RG.mdf;Integrated Security=True");
         SqlCommand cmd;
         SqlDataAdapter adapter;
@@ -24,89 +26,183 @@ namespace Kino_AB_V.A.TARpv23
         SaveFileDialog save;
         string extension;
 
+
         public void NaitaAndmed()
         {
             conn.Open();
             DataTable dt = new DataTable();
-            cmd = new SqlCommand("SELECT * FROM Toode", conn);
+            cmd = new SqlCommand("SELECT * FROM Filmi", conn);
             adapter = new SqlDataAdapter(cmd);
             adapter.Fill(dt);
-            dataGridView1.DataSource = dt;
+            dataGridView.DataSource = dt;
             conn.Close();
         }
 
-        private void Admin_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        
-
         public Admin()
         {
-            this.Height = 450;
+            this.Height = 550;
             this.Width = 800;
             this.Text = "Admin";
 
-            // dataGridView1
-            dataGridView1 = new DataGridView();
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(12, 208);
-            dataGridView1.Size = new Size(471, 230);
-             
-            // pictureBox_admin
-            pictureBox_admin = new PictureBox();
-            pictureBox_admin.Location = new Point(527, 78);
-            pictureBox_admin.Size = new Size(261, 360);
-             
-            // filmiNimi_txt
-            filmiNimi_txt = new TextBox();
-            filmiNimi_txt.Location = new Point(44, 33);
-            filmiNimi_txt.Size = new Size(100, 20);
+            // Filmi_pealkiri_lbl
+            Filmi_pealkiri_lbl = new Label();
+            Filmi_pealkiri_lbl.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            Filmi_pealkiri_lbl.Location = new Point(24, 22);
+            Filmi_pealkiri_lbl.Size = new Size(108, 20);
+            Filmi_pealkiri_lbl.Text = "Filmi pealkiri";
             
-            // filmiImg_txt
-            filmiImg_txt = new TextBox();
-            filmiImg_txt.Location = new Point(44, 126);
-            filmiImg_txt.Size = new Size(100, 20);
+            // Filmi_aasta_lbl
+            Filmi_aasta_lbl = new Label();
+            Filmi_aasta_lbl.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            Filmi_aasta_lbl.Location = new Point(24, 67);
+            Filmi_aasta_lbl.Size = new Size(96, 20);
+            Filmi_aasta_lbl.Text = "Filmi aasta";
+            
+            // filimi_nimi_txt
+            filimi_nimi_txt = new TextBox();
+            filimi_nimi_txt.Location = new Point(164, 22);
+            filimi_nimi_txt.Size = new Size(152, 20);
+            
+            // filmi_aasta_txt
+            filmi_aasta_txt = new TextBox();
+            filmi_aasta_txt.Location = new Point(164, 67);
+            filmi_aasta_txt.Size = new Size(152, 20);
+             
+            // lisa_poster_btn
+            lisa_poster_btn = new Button();
+            lisa_poster_btn.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold);
+            lisa_poster_btn.Location = new Point(485, 164);
+            lisa_poster_btn.Size = new Size(135, 44);
+            lisa_poster_btn.Text = "Lisa poster";
+            
+            // dataGridView
+            dataGridView = new DataGridView();
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.Location = new Point(28, 228);
+            dataGridView.Size = new Size(737, 210);
+            
+            // insert_btn
+            insert_btn = new Button();
+            insert_btn.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold);
+            insert_btn.Location = new Point(28, 164);
+            insert_btn.Size = new Size(136, 44);
+            insert_btn.Text = "Lisa andmed";
+            insert_btn.Click += Insert_btn_Click;
+            
+            // delete_btn
+            delete_btn = new Button();
+            delete_btn.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold);
+            delete_btn.Location = new Point(180, 164);
+            delete_btn.Size = new Size(136, 44);
+            delete_btn.Text = "Kustuta";
+            
+            // update_btn
+            update_btn = new Button();
+            update_btn.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold);
+            update_btn.Location = new Point(333, 164);
+            update_btn.Size = new Size(135, 44);
+            update_btn.Text = "Uuenda";
+            
+            // saal_lbl
+            saal_lbl = new Label();
+            saal_lbl.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold);
+            saal_lbl.Location = new Point(24, 112);
+            saal_lbl.Size = new Size(77, 20);
+            saal_lbl.Text = "Kinosaal";
+            
+            // comboBox1
+            comboBox1 = new ComboBox();
+            comboBox1.Location = new Point(164, 111);
+            comboBox1.Size = new Size(152, 21);
 
-            // filmiAasta_txt
-            filmiAasta_txt = new TextBox();
-            filmiAasta_txt.Location = new Point(44, 78);
-            filmiAasta_txt.Size = new Size(100, 20);
-            
-            // kustuta_btn
-            kustuta_btn = new Button();
-            kustuta_btn.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold);
-            kustuta_btn.Location = new Point(214, 33);
-            kustuta_btn.Size = new Size(100, 20);
-            kustuta_btn.Text = "Kustutamine";
-            kustuta_btn.Click += Kustuta_btn_Click;
-            
-            // lisa_btn
-            lisa_btn = new Button();
-            lisa_btn.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold);
-            lisa_btn.Location = new Point(214, 126);
-            lisa_btn.Size = new Size(100, 20);
-            lisa_btn.Text = "Lisamine";
-            lisa_btn.Click += Lisa_btn_Click;
-            
-            // uuenda_btn
-            uuenda_btn = new Button();
-            uuenda_btn.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Bold);
-            uuenda_btn.Location = new Point(214, 78);
-            uuenda_btn.Size = new Size(100, 20);
-            uuenda_btn.Text = "Uuendamine";
-            uuenda_btn.Click += Uuenda_btn_Click;
-            
-            // Add
-            Controls.Add(uuenda_btn);
-            Controls.Add(lisa_btn);
-            Controls.Add(kustuta_btn);
-            Controls.Add(filmiAasta_txt);
-            Controls.Add(filmiImg_txt);
-            Controls.Add(filmiNimi_txt);
-            Controls.Add(pictureBox_admin);
-            Controls.Add(dataGridView1);
+            Kinosaalid();
+            NaitaAndmed();
+            //
+            Controls.Add(comboBox1);
+            Controls.Add(saal_lbl);
+            Controls.Add(update_btn);
+            Controls.Add(delete_btn);
+            Controls.Add(insert_btn);
+            Controls.Add(dataGridView);
+            Controls.Add(lisa_poster_btn);
+            Controls.Add(filmi_aasta_txt);
+            Controls.Add(filimi_nimi_txt);
+            Controls.Add(Filmi_aasta_lbl);
+            Controls.Add(Filmi_pealkiri_lbl);
+        }
+
+       
+
+        private void Kinosaalid()
+        {
+            conn.Open();
+            cmd = new SqlCommand("SELECT Kinosaal_nimetus FROM Kinosaal WHERE Kinosaal_nimetus IN ('Kinosaal 1', 'Kinosaal 2')", conn);
+            adapter = new SqlDataAdapter(cmd);
+            kinosaal_table = new DataTable();
+            adapter.Fill(kinosaal_table);
+
+            comboBox1.Items.Clear();  // Clear any existing items in ComboBox
+
+            // Add cinema halls from the database (if they exist)
+            foreach (DataRow item in kinosaal_table.Rows)
+            {
+                comboBox1.Items.Add(item["Kinosaal_nimetus"]);
+            }
+
+            // If for some reason they aren't in the database, add them manually
+            if (!comboBox1.Items.Contains("Kinosaal 1"))
+            {
+                comboBox1.Items.Add("Kinosaal 1");
+            }
+            if (!comboBox1.Items.Contains("Kinosaal 2"))
+            {
+                comboBox1.Items.Add("Kinosaal 2");
+            }
+
+            conn.Close();
+        }
+        private void Insert_btn_Click(object sender, EventArgs e)
+        {
+            if (filimi_nimi_txt.Text.Trim() != string.Empty && filmi_aasta_txt.Text.Trim() != string.Empty && comboBox1.SelectedItem != null)
+            {
+                try
+                {
+                    conn.Open();
+
+                    // Get the selected cinema hall's ID
+                    cmd = new SqlCommand("SELECT Id FROM Kinosaal WHERE Kinosaal_nimetus=@kinosaal", conn);
+                    cmd.Parameters.AddWithValue("@kinosaal", comboBox1.SelectedItem.ToString());
+                    int kinosaalId = Convert.ToInt32(cmd.ExecuteScalar());
+
+                    // Check if the Kinosaal Id was retrieved successfully
+                    if (kinosaalId > 0)
+                    {
+                        cmd = new SqlCommand("INSERT INTO Filmi (Filmi_nimetus, Aasta, Poster, Kinosaal_Id) VALUES (@filmiNimetus, @aasta, @poster, @kinosaalId)", conn);
+                        cmd.Parameters.AddWithValue("@filmiNimetus", filimi_nimi_txt.Text);
+                        cmd.Parameters.AddWithValue("@aasta", filmi_aasta_txt.Text);
+                        cmd.Parameters.AddWithValue("@poster", filimi_nimi_txt.Text + extension);  // If extension is set
+                        cmd.Parameters.AddWithValue("@kinosaalId", kinosaalId);
+                        cmd.ExecuteNonQuery();
+
+                        MessageBox.Show("Film added successfully!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid cinema hall selected!");
+                    }
+
+                    conn.Close();
+                    NaitaAndmed();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error while adding data: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please fill in all fields and select a cinema hall.");
+            }
         }
 
         private void Uuenda_btn_Click(object sender, EventArgs e)
@@ -114,36 +210,11 @@ namespace Kino_AB_V.A.TARpv23
             throw new NotImplementedException();
         }
 
-        private void Lisa_btn_Click(object sender, EventArgs e)
-        {
-            if (filmiNimi_txt.Text.Trim() != string.Empty && filmiImg_txt.Text.Trim() != string.Empty && filmiAasta_txt.Text.Trim() != string.Empty)
-            {
-                try
-                {
-                    conn.Open();
-                    cmd = new SqlCommand("INSERT INTO Filmi (Filmi_nimetus, Aasta, Poster, Kinosaal_Id) VALUES (@filmiNimetus, @aasta, @poster, @kinosaalId)", conn);
-                    cmd.Parameters.AddWithValue("@filmiNimetus", filmiNimi_txt.Text);
-                    cmd.Parameters.AddWithValue("@aasta", filmiAasta_txt.Text);
-                    cmd.Parameters.AddWithValue("@poster", filmiNimi_txt.Text + extension);
-                    cmd.ExecuteNonQuery();
-
-                    conn.Close();
-                    NaitaAndmed();
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("Andmebaasiga viga");
-                }
-            }
-            else
-            {
-                MessageBox.Show("Sisesta andmeid");
-            }
-        }
-
         private void Kustuta_btn_Click(object sender, EventArgs e)
         {
             throw new NotImplementedException();
         }
+
+
     }
 }
